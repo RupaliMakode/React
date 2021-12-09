c6c71474df41cb15e00f0cbb6455a40ce8d5633d
RupaliMakode <92510731+RupaliMakode@users.noreply.github.com>
2021-12-09 17:30:43 +0530

    Add files via upload

---
+++ userhookssample src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+import UserHooksSample from './UserHooksSample';
+function App() {
+  
+  return (
+    <div className="App">
+      {/* <h1>Sample App Using Hooks</h1>
+       */}
+      {/* <RefParent/> */}
+      {/* I am inside App Component */}
+      {/* <PortalDemo/> */}
+      {/* <ProfilerDemo/> */}
+      {/* <HooksSample/>  */}
+      <h1>From HookSample</h1>
+      <UserHooksSample/>
+    </div>
+  );
+}
+
+export default App;
